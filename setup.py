68163fdf0baa7759e53b09b4fbbe42a640ee7d43
sachint97 <cssachinshoba@gmail.com>
2024-04-25 02:16:43 +0530

    notebook experiment added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="wasteDetection",
+    version="0.0.0",
+    author="Sachin.T",
+    author_email="cssachinshoba@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
