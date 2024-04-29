3a4b4b0985f098cda44230a819bc814e8d815a7b
sachint97 <cssachinshoba@gmail.com>
2024-04-30 05:25:50 +0530

    prediction completed

--- wasteDetection/utils/main_utils.py
+++ wasteDetection/utils/main_utils.py
@@ -38,7 +38,7 @@ def decodeImage(imgstring, filename):
     imgdata = base64.b64decode(imgstring)
     with open("./data/"+ filename, "wb") as f:
         f.write(imgdata)
-        f.colse()
+        f.close()
 
 def encodeImageIntoBase64(cropped_image_path):
     with open(cropped_image_path, "rb") as f:
