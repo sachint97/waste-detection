9cc8e07997f1c0b95dbc0976c9b9b35a1bf8cb36
sachint97 <cssachinshoba@gmail.com>
2024-04-30 00:17:00 +0530

    data validation completed

--- wasteDetection/pipeline/training_pipeline.py
+++ wasteDetection/pipeline/training_pipeline.py
@@ -7,9 +7,14 @@ from wasteDetection.components.data_ingestion import DataIngestion
 from wasteDetection.entity.config_entity import DataIngestionConfig
 from wasteDetection.entity.artifact_entity import DataIngestionArtifact
 
+from wasteDetection.entity.artifact_entity import DataValidationArtifact
+from wasteDetection.entity.config_entity import DataValidationConfig
+from wasteDetection.components.data_validation import DataValidation
+
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -34,10 +39,32 @@ class TrainPipeline:
             logging.error(error)
             raise error
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info("Entered the data validation pipeline")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info("Exited the start data validation method of training pipeline")
+
+            return data_validation_artifact
+        
+        except Exception as e:
+            error = AppException(e,sys)
+            logging.error(error)
+            raise error
+
     def run_pipeline(self)->None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            
         except Exception as e:
             error = AppException(e,sys)
             logging.error(error)
